e26818c8414ece50f53b5c166b609c283debd83a
Kshitij Sinha <kshitij.avi.s@gmail.com>
2021-08-03 13:37:30 -0500

    Add GET request to get actual list of mentors

--- src/components/mentors-list.js
+++ src/components/mentors-list.js
@@ -2,22 +2,20 @@ import { useState, useEffect } from 'react';
 import MentorBio from './mentor-bio';
 
 function MentorsList() {
-    const fixedMentorData = [
-        {
-           "description":"Pablo is a computer scientist working as a lead software engineer at MindYourRights, where he's co-authoring a disruptive, common platform for global music rights management. He's passionate about computer programming, cycling, and coffee. He's been an active open source contributor for at least 15 years, writing mostly in Python, Go, and JavaScript. For the last few years, he found himself in love with Elixir and Rust. He's very excited to return to HackIllinois as a mentor for the 4th consecutive year!",
-           "firstName":"Pablo",
-           "lastName":"Aguiar",
-           "profile":"https://hackillinois-upload.s3.amazonaws.com/photos/mentors/pablo_aguiar.png"
-        },
-        {
-           "description":"Gino Corrales is a developer team lead for Caterpillar Inc. in the Cybersecurity Division. Gino was always passionate for mobile wearables, security and software development technologies. Prior to joining Caterpillar, Gino was a mobile developer for State Farm at the University of Illinois Research Park and was responsible for leading a group of interns for almost two years. Gino earned a Computer Science degree from Illinois State University and realized studies of Systems Engineering back at his hometown's university - Universidad Catolica de Santa Maria - in Arequipa, Peru.",
-           "firstName":"Gino",
-           "lastName":"Corrales",
-           "profile":"https://hackillinois-upload.s3.amazonaws.com/photos/mentors/gino_corrales.jpg"
-        },
-    ]
-    
-    const [mentors, setMentors] = useState(fixedMentorData)
+    const [mentors, setMentors] = useState([])
+    const [error, setError] = useState(null)
+
+    useEffect (() => {
+        fetch("https://api.hackillinois.org/upload/blobstore/mentors/")
+            .then(result => result.json())
+            .then(
+                (result) => {
+                    console.log(result);
+                    setMentors(result.data);
+                },
+                (error) => {setError(error)}
+            )
+    });
 
     const mentorBios = mentors.map((mentor) =>
         <li>
@@ -27,12 +25,17 @@ function MentorsList() {
         </li>
     );
 
-    return (
-        <div>
-            <h1>Meet our Mentors</h1>
-            <ul>{mentorBios}</ul>
-        </div>
-    )
+    // Rendering
+    if (error) {
+        return (<div>Error: {error.message}</div>)
+    } else {
+        return (
+            <div>
+                <h1>Meet our Mentors</h1>
+                <ul>{mentorBios}</ul>
+            </div>
+        )
+    }
 }
 
 export default MentorsList;
