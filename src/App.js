8a030ad6542f046eb56cfbb67d0f4751e30823a3
Kshitij Sinha <kshitij.avi.s@gmail.com>
2021-08-03 11:30:24 -0500

    Create list of mentor names with hardcoded data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import MentorsList from './components/mentors-list';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       </header>
+      <MentorsList></MentorsList>
     </div>
   );
 }
